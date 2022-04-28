dd5191178c9187cf8feec5fe1f71d9c72d85b805
DNationDev <104176404+DNationDev@users.noreply.github.com>
2022-04-27 20:54:18 -0700

    added slider
    added a slider with buttons in the top-contianer div

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 const eventTitles = document.querySelectorAll('.event-title');
 const events = document.querySelectorAll('.event');
 const article = document.querySelector('.article')
-
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const slides = document.querySelectorAll('.slide')
 
 article.addEventListener('click', e =>{
     let id = e.target.dataset.id;
@@ -23,4 +25,36 @@ article.addEventListener('click', e =>{
         let element = document.getElementById(id); 
         element.classList.add('active')
     }
+})
+
+let counter = 0;
+
+const slider = () => {
+    if(counter === slides.length){
+        counter = 0;
+
+    }
+    
+    if(counter < 0) {
+        counter = slides.length -1;
+    }
+
+    slides.forEach(slide =>{
+        console.log(slide)
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    })
+}
+
+
+
+
+
+prevBtn.addEventListener('click', e =>{
+    counter --;
+    slider();
+})
+
+nextBtn.addEventListener('click', e =>{
+    counter ++;
+    slider();
 })
