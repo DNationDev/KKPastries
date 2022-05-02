dc00bdf3a0843155ef2e8748c877923c0b6aace2
DNationDev <104176404+DNationDev@users.noreply.github.com>
2022-05-01 23:57:33 -0700

    small changes

--- script.js
+++ script.js
@@ -3,9 +3,34 @@
 const eventTitles = document.querySelectorAll('.event-title');
 const events = document.querySelectorAll('.event');
 const article = document.querySelector('.article')
+
 const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
 const slides = document.querySelectorAll('.slide')
+
+const search = document.getElementById('search');
+const searchIcon = document.querySelector('.search-icon');
+const searchContainer = document.querySelector('.search-container')
+
+
+let products = [{
+    name: 'lemon cookies',
+    id: 1}, {
+    name: 'strawberry crunch cookies',
+    id: 2}, {
+    name: 'chocolate chip cookies',
+    id: 3}, {
+    name: 'shortbread cookies',
+    id: 4}, {
+    name: 'trolls cake',
+    id: 5}, {
+    name: 'crown cake',
+    id: 6}, { 
+    name: 'henny cake',
+    id: 7}
+];
+
+
 
 article.addEventListener('click', e =>{
     let id = e.target.dataset.id;
@@ -58,4 +83,17 @@ prevBtn.addEventListener('click', e =>{
 nextBtn.addEventListener('click', e =>{
     counter ++;
     slider();
+})
+
+
+searchContainer.addEventListener('click', e =>{
+    let input = e.target.value.toLowerCase();
+    products.forEach(item =>{
+        console.log(item)
+        let isVisible = item.name.toLowerCase().includes(input);
+        if(isVisible){
+            console.log(item.id)
+        }
+    })
+    console.log(input);
 })
